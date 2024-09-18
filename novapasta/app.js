deb052622859f77d285797a95421767ba48e10ec
kauan01011 <kauannchefen@gmail.com>
2024-09-17 22:21:44 -0300

    novo commit

--- novapasta/app.js
+++ novapasta/app.js
@@ -1,15 +1,28 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 const port = 8080
+const path = require('path');
 
-function lidarComRequisicao(Req, Res){
-    Res.send("teste")
-}
 
-app.get('/sobre', (req, res) =>{
-    res.send("<h1>titulo</h1>")
+// Serve the 'index.html' file from the root
+
+// Middleware para habilitar o parsing de JSON no body
+app.use(express.json());
+
+app.get('/api-tester', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/', (req, res) => {
+  res.send('Funcionou essa p****!')
 })
 
-app.listen(port, ()=>{
-    console.log("ta rodando")
+//Cole sua rota POST aqui
+app.post('/', (req, res) => {
+    console.log('Dados recebidos no body:', req.body);
+    res.json({ message: 'Dados recebidos com sucesso!', body: req.body });
+})
+
+app.listen(port, () => {
+console.log(`Example app listening on port ${port}`)
 })
